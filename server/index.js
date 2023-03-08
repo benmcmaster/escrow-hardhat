58a0edf7973b14a056cc367b7eec65af8c5ad9b6
Ben McMaster <ben@benmcmaster.com>
2023-03-07 16:31:38 -0800

    changes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 const express = require('express')
-const cors = require('cors');
+const cors = require('cors')
+require('dotenv').config()
+const mysql = require('mysql2')
 
 const app = express()
 const port = 3001
+
+const connection = mysql.createConnection(process.env.DATABASE_URL)
+console.log('Connected to PlanetScale!')
+connection.end()
 
 app.use(cors({
     origin: '*'
